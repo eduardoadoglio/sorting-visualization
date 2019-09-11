593c53337944968e97ab8b40fb5e50156c5442c5
eduardoadoglio <eduardoadoglio4@gmail.com>
2019-09-11 15:47:09 -0300

    Add quicksort algorithm

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const b = ( bubble ) => {
     let i = 0;
     let j = 0;
     bubble.setup = () => {
-        bubble.createCanvas(1000,500);
+        bubble.createCanvas(100,50);
         bubble.background(200);
         for(k = 0; k < bubble.width; k++){
             val.push(Math.floor((Math.random() * bubble.height) + 1));
@@ -34,27 +34,61 @@ const b = ( bubble ) => {
             console.log("sorted:");
         }
         bubble.background(50);
+        bubble.stroke(255);
         for(k = 0; k < val.length; k++){
-            bubble.stroke(255);
             bubble.line(k, bubble.height, k, bubble.height-val[k]);
         }
     }
 }
 
-let bubble = new p5(b, 'bubble')
+let bubble = new p5(b, 'bubble');
 
 // Quick Sort Canvas
 
 const q = ( quick ) => {
-    
+    let val = [];
     quick.setup = () => {
-        quick.createCanvas(200,200);
+        quick.createCanvas(100,50);
         quick.background(0);
+        for(k = 0; k < quick.width; k++){
+            val.push(Math.floor((Math.random() * quick.height) + 1));
+        }
+        quick.quickSort(val, 0, val.length-1);
     }
 
     quick.draw = () => {
+        quick.background(50);
+        quick.stroke(255);
+        for(i = 0; i < val.length; i++){
+            quick.line(i, quick.height, i, quick.height-val[i]);
+        }
+    }
 
+    quick.quickSort = (arr, start, end) => {
+        if(start >= end) return;
+
+        let index = quick.partition(arr, start, end);
+
+        quick.quickSort(arr,start, index-1);
+        quick.quickSort(arr, index+1, end);
+    }
+
+    quick.partition = (arr, start, end) => {
+        let pivotIndex = start;
+        let indexValue = arr[end];
+        for(i = start; i < end; i++){
+            if(arr[i] < indexValue){
+                let temp = arr[i];
+                arr[i] = arr[pivotIndex];
+                arr[pivotIndex] = temp;
+                pivotIndex++;
+            }
+        }
+        let temp = arr[pivotIndex];
+        arr[pivotIndex] = arr[end];
+        arr[end] = temp;
+        return pivotIndex;
     }
 }
 
-let quick = new p5(q, 'quick')
+let quick = new p5(q, 'quick');
